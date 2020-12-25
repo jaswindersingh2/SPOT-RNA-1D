4b463f9fcd48fbb78db32d278862a21d609d6754
jaswindersingh2 <jaswinder.singh3@griffithuni.edu.au>
2020-12-25 18:18:25 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# SPOT-RNA-1D
-RNA backbone torsion and pseudotorsion angle prediction using dilated convolutional neural networks
+SPOT-RNA-1D: *RNA backbone torsion and pseudotorsion angle prediction using dilated convolutional neural networks.*
+====
+
+System Requirments
+----
+
+**Hardware Requirments:**
+SPOT-RNA-1D predictor requires only a standard computer with around 32 GB RAM to support the in-memory operations for RNAs sequence length less than 2000.
+
+**Software Requirments:**
+* [Python3](https://docs.python-guide.org/starting/install3/linux/)
+* [virtualenv](https://virtualenv.pypa.io/en/latest/installation/) or [Anaconda](https://anaconda.org/anaconda/virtualenv)
+* [CUDA 10.0](https://developer.nvidia.com/cuda-10.0-download-archive) (Optional If using GPU)
+* [cuDNN (>= 7.4.1)](https://developer.nvidia.com/cudnn) (Optional If using GPU)
+
+SPOT-RNA-1D has been tested on Ubuntu 14.04, 16.04, and 18.04 operating systems.
+
+
+Installation
+----
+
+To install SPOT-RNA-1D and it's dependencies following commands can be used in terminal:
+
+1. `git clone https://github.com/jaswindersingh2/SPOT-RNA-1D.git && cd SPOT-RNA-1D`
+2. `wget -O checkpoints.tar.xz 'https://www.dropbox.com/s/r9hp20gk30unptf/checkpoints.tar.xz' || wget -O checkpoints.tar.xz 'https://app.nihaocloud.com/f/4d2385c633554ccaa85c/?dl=1'`
+3. `tar -xvf checkpoints.tar.xz && rm checkpoints.tar.xz`
+
+Either follow **virtualenv** column steps or **conda** column steps to create virtual environment and to install SPOT-RNA-1D dependencies given in table below:<br />
+
+|  | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; virtualenv | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; conda |
+| :- | :-------- | :--- |
+| 4. | `virtualenv -p python3.6 venv` | `conda create -n venv python=3.6` |
+| 5. | `source ./venv/bin/activate` | `conda activate venv` | 
+| 6. | *To run SPOT-RNA-1D on CPU:*<br /> <br /> `pip install tensorflow==1.15.0` <br /> <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *or* <br /> <br />*To run SPOT-RNA-1D on GPU:*<br /> <br /> `pip install tensorflow-gpu==1.15.0` | *To run SPOT-RNA-1D on CPU:*<br /> <br /> `conda install tensorflow==1.15.0` <br /> <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *or* <br /> <br />*To run SPOT-RNA-1D on GPU:*<br /> <br /> `conda install tensorflow-gpu==1.15.0` |
+| 7. | `pip install -r requirements.txt` | `while read p; do conda install --yes $p; done < requirements.txt` | 
+
+Usage
+----
+
+**To run the SPOT-RNA-1D**
+
+```
+python3 run.py --seq_file inputs/TS1_seqs.fasta --save_outputs outputs/
+```
+
+Datasets
+----
+
+The following dataset was used for Training, Validation, and Testing of SPOT-RNA-1D:
+
+[Dropbox](https://www.dropbox.com/s/fl1upqsvd7rpyrl/RNAsnap2_data.zip) or [Nihao Cloud](https://app.nihaocloud.com/f/afea8e005a964bf8bb0f/)
+
+Citation guide
+----
+
+**If you use SPOT-RNA-1D for your research please cite the following paper:**
+
+Singh, J., Paliwal, K., Singh, J., Zhou, Y., 2021. RNA backbone torsion and pseudotorsion angle prediction using dilated convolutional neural networks.
+
+Licence
+----
+Mozilla Public License 2.0
+
+
+Contact
+----
+jaswinder.singh3@griffithuni.edu.au, yaoqi.zhou@griffith.edu.au
+
---
+++ run.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+import numpy as np
+import os, argparse, tqdm
+import time
+start = time.time()
+from argparse import RawTextHelpFormatter
+
+start = time.time()
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser(description='SPOT-RNA-1D: RNA backbone torsion angle prediction', usage='use "python %(prog)s --help" for more information', formatter_class=RawTextHelpFormatter)
+parser.add_argument('--seq_file',default=base_path + '/inputs/TS1_seqs.fasta', type=str, help="Path to the input sequence fasta file\n" "default file: " + base_path + "/inputs/TS1_seqs.fasta", metavar='')
+parser.add_argument('--save_outputs',default=base_path + '/outputs', type=str, help="Path to the folder for saving output\n" "default folder: " + base_path + "/outputs", metavar='')
+parser.add_argument('--batch_size',default=10, type=int, help="Number of simultaneous prediction for multi sequence fasta file input\n" "default batch size: 10", metavar='')
+parser.add_argument('--gpu', default=-1, type=int, help="To run on GPU, specifiy GPU number. If only one GPU in the system specifiy 0\n" "default: -1 (no GPU)\n", metavar='')
+args = parser.parse_args()
+
+class bcolors:
+    RED   = "\033[1;31m"
+    BLUE  = "\033[1;34m"
+    CYAN  = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD    = "\033[;1m"
+    REVERSE = "\033[;7m"
+
+
+########## mean and standard deviation of trainin data TR1 ####################
+norm_mu = [0.24416392, 0.19836862, 0.30642843, 0.24948534]
+norm_std = [0.43031312, 0.39954973, 0.46168336, 0.43343267]
+
+
+print(bcolors.CYAN + '\nReading input seqeunce file from ' + args.seq_file + bcolors.RESET)
+
+############## read input file #####################
+with open(args.seq_file) as file:
+    input_data = [line.strip() for line in file.read().splitlines() if line.strip()]
+count = int(len(input_data)/2)  # count number of input sequences
+
+ids = [''.join(e if e.isalnum() else '_' for e in input_data[2*i].replace(">", "")) for i in range(count)]   # name of each input sequence after ignoring by special characters.
+
+###### extracting all the input sequences  #############
+sequences = {}
+for i,I in enumerate(ids):
+    sequences[I] = input_data[2*i+1].replace(" ", "").upper().replace("T", "U")  # removing spaces, converting lowercase to uppercase, and replacing T with U
+
+bases = np.array([base for base in 'AUGC'])
+
+##### one-hot encoding of the input sequence and saving into a dictionary (feat_dic) #########
+feat_dic = {}
+for i,I in enumerate(ids):
+	feat_dic[I] = np.concatenate([[(bases==base.upper()).astype(int)] if str(base).upper() in 'AUGC' else np.array([[0]*4]) for base in sequences[I]])  # one-hot encoding Lx4
+
+
+########## GPU setting ##############
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+os.environ["CUDA_VISIBLE_DEVICES"]= str(args.gpu)
+
+
+
+def get_data(sample_feat, ids, batch_size, i, norm_mu,norm_std):
+	###############################################
+	# prepare normalize input feature
+	# make batch of input features
+    # prepare zero-mask for different length input sequences
+	###############################################
+    data = [(sample_feat[j][:,:]-norm_mu)/norm_std for j in ids[i * batch_size:np.min([(i + 1) * batch_size, len(ids)])]] 
+    data = [np.concatenate([np.ones((j.shape[0], 1)), j], axis=1) for j in data]
+    seq_lens = [j.shape[0] for j in data]
+    batch_ids = [j for j in ids[i * batch_size:np.min([(i + 1) * batch_size, len(ids)])]]
+    max_seq_len = max(seq_lens)
+    data = np.concatenate([np.concatenate([j, np.zeros((max_seq_len - j.shape[0], j.shape[1]))])[None, :, :] for j in data])
+
+    mask = np.concatenate([np.concatenate([np.ones((1, seq_lens[j])), np.zeros((1, max_seq_len - seq_lens[j]))], axis=1) for j in range(len(ids[i * batch_size:np.min(((i + 1) * batch_size, len(ids)))]))])
+
+    return data, mask, seq_lens, batch_ids
+
+def sigmoid(x):
+  return x#1 / (1 + np.exp(-x))
+
+
+##### soft placing of tensorflow model 
+config = tf.compat.v1.ConfigProto()
+config.allow_soft_placement=True
+config.log_device_placement=False
+
+print(bcolors.GREEN + '\nRunning SPOT-RNA-1D\n' + bcolors.RESET)
+
+outputs = {}
+with tf.compat.v1.Session(config=config) as sess:
+    saver = tf.compat.v1.train.import_meta_graph(base_path + '/checkpoints/tensorflow_model.meta')
+    saver.restore(sess,base_path + '/checkpoints/tensorflow_model')
+    graph = tf.compat.v1.get_default_graph()
+    tmp_out = graph.get_tensor_by_name('output_FC/fully_connected/BiasAdd:0')
+
+    for batch_test in tqdm.tqdm(range(max(1,int(np.ceil(count/args.batch_size))))):
+        feature, mask, seq_lens, batch_ids = get_data(feat_dic, ids, batch_size=args.batch_size, i=batch_test, norm_mu=norm_mu, norm_std=norm_std)
+        out = sess.run([tmp_out],feed_dict={'input_feature:0': feature, 'seq_lens:0':seq_lens, 'zero_mask:0':mask, 'keep_prob:0':1.})
+        pred_angles = sigmoid(out[0])
+        for i, id in enumerate(batch_ids):
+            outputs[id] = pred_angles[i,0:seq_lens[i]]
+tf.compat.v1.reset_default_graph()
+
+for id in ids:
+
+	seq = np.array([[i,I] for i,I in enumerate(sequences[id])])
+
+	preds = outputs[id] * 2 - 1
+	preds_angle_rad = [np.arctan2(preds[:,2*i], preds[:,2*i+1]) for i in range(int(preds.shape[1]/2))]
+	preds_angle = np.round(np.transpose(np.degrees(preds_angle_rad)), 2)
+	final_output =  np.concatenate((seq, preds_angle), axis=1)
+	np.savetxt(os.path.join(args.save_outputs, str(id.split('.')[0]))+'.txt', (final_output), delimiter='\t\t', fmt="%s", header='No.\t   Seq\t\t Alpha\t\t Beta\t\tGamma\t\tDelta\t\t Epsilon\t Zeta\t\t  Chi\t\t  Eta\t\t Theta' + '\n', comments='')
+
+print(bcolors.GREEN + '\nFinished!' + bcolors.RESET)
+print(bcolors.GREEN + 'SPOT-RNA-1D prediction saved in folder ' + args.save_outputs + bcolors.RESET)
+
+end = time.time()
+print(bcolors.CYAN + '\nProcesssing Time {:.4f} seconds\n'.format(end - start) + bcolors.RESET)
+
